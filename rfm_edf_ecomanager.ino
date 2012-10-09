d3957bae201b1cb83d48701246a6fde034e97a85
Jack Kelly <jack-list@xlk.org.uk>
2012-10-09 22:36:49 +0200

    Simplify .ino file
    All those silly includes turned out not to be necessary to get the Windows ArduinoIDE to compile.

--- rfm_edf_ecomanager.ino
+++ rfm_edf_ecomanager.ino
@@ -1,10 +1,2 @@
-/* These includes are necessary to force the Arduino IDE to compile
- * and link all the necessary files.
- * This technique was taken from http://provideyourown.com/2011/advanced-arduino-including-multiple-libraries/ */
-#include "spi.h"
-#include "Packet.h"
-#include "Rfm12b.h"
-#include "Manager.h"
-
 /* Include the actual main function for the code: */
 #include "rfm_edf_ecomanager.cpp"
